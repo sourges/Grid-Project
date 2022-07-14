be4873cfff171106a3e27ebf5752e02507800173
sourges <107276414+sourges@users.noreply.github.com>
2022-07-14 18:20:43 -0400

    Add files via upload
    updated

--- kucoin_dca.py
+++ kucoin_dca.py
@@ -1,7 +1,11 @@
 # testing code for DCA bot
 
-# - to do list for 7/14
-# - work on main loop for TP
+# - to do list for 7/15
+
+# - get 'available funds' from account - maybe 7/16
+# - test main loop - as of 7/14 not enough time to see actual testing if it works
+
+# - secondary items for 7/15 - probably for next day
 # - start thinking of a function that will find out - priceIncrement + baseMinSize for base order - for limit at least - see what difference market does - symbol_list has this, see if you can
 #   just call one at a time
 # - priceIncrement + baseMinSize would be used to round() everything
@@ -24,13 +28,13 @@ price_deviation_percent = .01 # 1% - normally run .005 or .5%
 
 saftey_order_volume_scale = 2 # multiples orders by - in this case - * 2
 
-funds = 38.19664711  # will need to retreive actual available balance
+funds = 35.59847051  # will need to retreive actual available balance
 
 
 # testing these right now
 
-base_order_percent =  .01 # 1%
-saftey_order_percent = .01 # 1%
+base_order_percent =  .02 # 1%
+saftey_order_percent = .02 # 1%
 
 initial_order_buy_amount = funds * base_order_percent # only called once through market order
 initial_safety_buy_amount = funds * saftey_order_percent
@@ -44,6 +48,8 @@ tp_orders = []
 # initial buy - eventually from webhook
 
 def dca_bot(initial_safety_buy_amount):
+	order_amount = []
+	order_cost = []
 
 	# might also need a try / except to make sure the order is actually placed before any dca or tp are placed
 	order_Id = place_market_order(round(initial_order_buy_amount,6))  # can move this round somewhere else - main issue is its different for each base - market only quoteIncrement
@@ -53,12 +59,17 @@ def dca_bot(initial_safety_buy_amount):
 	initial_price, initial_fee, order_quantity = test_fills(order_id)  # returns from market price
 	last_deviation = 0
 
+	#testing order_amount + cost for dca
+	order_amount.append(order_quantity)
+	order_cost.append(round(initial_order_buy_amount,6))
+
 	# test purposes, will delete later
 	print(f"price inside of dca_bot - {initial_price}")
 	print(f"market buy fee - {initial_fee}")
 	print(f"order quantity - {order_quantity}")
 
 	tp_price_with_fee = ((initial_price + initial_fee) * take_profit_percent) + initial_price
+	
 
 	# print for testing purposes
 	print(f"tp_price_with_fee - {tp_price_with_fee}")
@@ -68,11 +79,13 @@ def dca_bot(initial_safety_buy_amount):
 
 	tp_orders.append(take_profit_order) # should have to ['data'] but on next buy test look at print
 
+	print("testing - tp_orders")
+	print(tp_orders)
 
 	# ****** currently testing ************
 
 
-
+	
 	for i in range(max_safety_orders):
 		deviation = price_deviation_percent + last_deviation
 		deviation = round(deviation, 2)
@@ -80,6 +93,12 @@ def dca_bot(initial_safety_buy_amount):
 		deviated_price = round(deviated_price, 7)  # will be limit price
 
 		base_limit_order = initial_safety_buy_amount / deviated_price # - limit needs a base order to buy (Eth in this case)
+
+		# testing
+		order_amount.append(base_limit_order)
+		order_cost.append(initial_safety_buy_amount)
+
+
 		dca_buys = place_limit_order(deviated_price, base_limit_order, "BUY")
 		dca_orders.append(dca_buys)
 
@@ -93,9 +112,10 @@ def dca_bot(initial_safety_buy_amount):
 		initial_safety_buy_amount *= saftey_order_volume_scale 
 		initial_safety_buy_amount = round(initial_safety_buy_amount, 7)
 
-		last_deviation = deviation * safety_order_step_scale
 
-	return dca_orders, tp_orders # dca orders which have order ids in an array, tp_orders only has one in an array
+		last_deviation = deviation * safety_order_step_scale
+		time.sleep(1)
+	return dca_orders, tp_orders, order_amount, order_cost # dca orders which have order ids in an array, tp_orders only has one in an array
 
 
 def call_code(str_to_sign ,data_json=None, order_id=None):
@@ -117,14 +137,7 @@ def call_code(str_to_sign ,data_json=None, order_id=None):
 
 
 
-# once TP is hit, all SO need to be cancelled
-# will not use cancel all from api due to other trades in progress
 
-def cancel_orders():
-	# since orders will be in an array, will make a 'for' loop to cycle through oderID and delete
-	url = 'https://api.kucoin.com/api/v1/orders/' + order_id
-	now = int(time.time() * 1000)
-	str_to_sign = str(now) + 'DELETE' + '/api/v1/orders/' + order_id
 
 
 
@@ -136,9 +149,11 @@ def get_order_info(order_id):
 	data_json = None
 	HEADERS = call_code(str_to_sign ,data_json, order_id)
 	response = requests.get(url, headers = HEADERS)
-	print(response.status_code)
-	print(response.json())
+	# print("order info method")
+	# print(response.status_code)
+	# print(response.json())
 	return response.json()
+
 
 
 # used to get order info - MARKET ONLY - working
@@ -231,13 +246,136 @@ def account_info():
 #print(funds)
 
 
-dca_orders, tp_orders = dca_bot(initial_safety_buy_amount)  # - main for now
+
+#print(f"testing - dca_orders {dca_orders}")
+#print(f"testing - tp_orders {tp_orders}")
 
 
-print(f"testing - dca_orders {dca_orders}")
-print(f"testing - tp_orders {tp_orders}")
+
+def get_order_list():
+	url = 'https://api.kucoin.com/api/v1/orders?status=active'
+	now = int(time.time() * 1000)
+	str_to_sign = str(now) + 'GET' + '/api/v1/orders?status=active'
+	HEADERS = call_code(str_to_sign)
+	response = requests.get(url, headers = HEADERS)
+	print(response.status_code)
+	print(response.json()['data']['items'])
+
+
+#get_order_list()
+
+# once TP is hit, all SO need to be cancelled
+# will not use cancel all from api due to other trades in progress
+
+def cancel_orders(order_id):
+	# since orders will be in an array, will make a 'for' loop to cycle through oderID and delete
+	url = 'https://api.kucoin.com/api/v1/orders/' + order_id
+	now = int(time.time() * 1000)
+	str_to_sign = str(now) + 'DELETE' + '/api/v1/orders/' + order_id
+	data_json = None
+	HEADERS = call_code(str_to_sign, data_json, order_id)
+	response = requests.delete(url, headers = HEADERS)
+
+
 
 
 # main loop here
 
+dca_orders, tp_orders, order_amount, order_cost = dca_bot(initial_safety_buy_amount)  # - main for now
+
+# for testing purpose
+count = 1 
+
+
+# total cost / total amount
+
+total_order_cost = order_cost[0]
+total_order_amount = order_amount[0]
+
+# just for test, will delete later
+average_cost = order_cost[0] / order_amount[0]
+print(f"testing average cost - {average_cost}")
+
+# keep
+del order_cost[0]
+del order_amount[0]
+
+
 while True:
+
+	# if the tp has been filled, delete all dca orders
+	for tp_order in tp_orders:  # is this necessary if there is always only one TP?
+		time.sleep(5) # needed at all?
+		print("checking TP")
+		order = get_order_info(tp_order['orderId'])
+		print(order)
+		print(f"TP status is - {order['data']['isActive']}")
+		if order['data']['isActive'] == closed_order_status:
+			for dca_order in dca_orders:
+				print("TP HIT - CANCELLING ORDERS!!")
+				cancel_orders(dca_order['orderId'])
+				print(f"cancelling orderID - {dca_order['orderId']}")
+				time.sleep(1)
+
+			# quit() or some function to close program?  for now only testing 1 test at a time and not 2+
+
+		# close all dca orders - this currently works
+
+	print(f"testing order_amount array - {order_amount}")
+	print(f"testing order_cost array - {order_cost}")
+
+	order = get_order_info(dca_orders[0]['orderId'])
+
+	print(f"general tp_orders test - {tp_orders[0]['orderId']}")  # testing to see if correct to cancel order
+
+	# test try / except since 'if order['data']['isActive'] == closed_order_status' bugs out once in awhile
+	
+	try:
+		if order['data']['isActive'] == closed_order_status:
+			print(f"current count - {count} - dca order")
+			count += 1
+			
+
+			total_order_cost += order_cost[0]
+			total_order_amount += order_amount[0]
+			average_cost = total_order_cost / total_order_amount
+			del order_cost[0]
+			del order_amount[0]
+
+			cancel_orders(tp_orders[0]['orderId'])  # gave an error last time, but tp was already hit.  need to test this when DCA is hit - probably need tp_orders[0]['orderId']
+
+			print("cancelling TP order")
+			del tp_orders[0]
+			print(f"empty here? - {tp_orders}")
+
+
+			print(f"new order_cost after TP hit - {order_cost}")
+			print(f"new order_amount after TP hit - {order_amount}")
+			print(f"new average cost - {average_cost}")
+			
+			tp_price = (average_cost * .003) + average_cost
+			print(f"TP price = {tp_price}")
+
+			print("new TP order")
+			take_profit_order = place_limit_order(tp_price, total_order_amount, "SELL" )
+			print(take_profit_order)
+
+
+			print("deleting 1st [0] dca order")
+			del dca_orders[0]
+			print("checking dca array")
+			print(dca_orders)
+
+
+			# new TP here
+			tp_orders.append(take_profit_order)
+			print(f"add new TP order to array - {tp_orders}")
+	except Exception as e:
+		print("request failed")
+		continue
+
+
+
+
+
+
